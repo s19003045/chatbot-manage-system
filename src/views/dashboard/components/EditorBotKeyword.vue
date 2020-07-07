cf965c51b1f3b52784524757f6bb40d4fd29824a
Gary Wu <s19003045@gmail.com>
2020-07-07 19:11:26 +0800

    關鍵字回應模組-前端-取得之模組清單資料渲染至 component 頁面

--- src/views/dashboard/components/EditorBotKeyword.vue
+++ src/views/dashboard/components/EditorBotKeyword.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class>
-    <h1>EditorBotKeyword</h1>
-    <ModuleList />
-    <EventEditor />
-    <ReplyMsgEditor />
+  <div class="my-2">
+    <h3>關鍵字回應</h3>
+    <div class="container">
+      <div class="row my-4">
+        <div class="col col-2">
+          <ModuleList :module-keywords="moduleKeywords" />
+        </div>
+        <div class="col col-5">
+          <EventEditor />
+        </div>
+        <div class="col col-5">
+          <ReplyMsgEditor />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +25,7 @@ import ReplyMsgEditor from "./components/EditorBotKeyword/ReplyMsgEditor.vue";
 
 // import helpers
 import keywordReplyAPI from "../../../apis/keywordReply.js";
+import { Toast } from "../../../utils/helpers";
 
 export default {
   name: "EditorBotKeyword",
@@ -43,9 +54,20 @@ export default {
       if (statusText === "OK") {
         console.log("response:", data);
         this.moduleKeywords = data.data.moduleKeywords;
+
+        Toast.fire({
+          icon: "success",
+          title: "成功取得資料",
+          text: ""
+        });
       }
     } catch (err) {
       console.log(err);
+      Toast.fire({
+        icon: "error",
+        title: "取得資料失敗，請稍後再試",
+        text: `${err.message}`
+      });
     }
   },
   computed: {},
--- src/views/dashboard/components/components/EditorBotKeyword/ModuleList.vue
+++ src/views/dashboard/components/components/EditorBotKeyword/ModuleList.vue
@@ -1,7 +1,36 @@
 <template>
-  <h1>ModuleList.vue</h1>
+  <div class="mb-5 py-3 px-1 border border-secondary rounded">
+    <h5>關鍵字回應模組清單</h5>
+    <table class="table table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">模組名稱</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="moduleKeyword in moduleKeywords" v-bind:key="moduleKeyword.id">
+          <td
+            :data-module-keyword-uuid="moduleKeyword.uuid"
+          >{{moduleKeyword.name === null ? "尚未命名模組" : moduleKeyword.name}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <hr />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ModuleList",
+  props: {
+    moduleKeywords: {
+      type: Array
+    }
+  },
+  data() {
+    return {};
+  },
+  created() {}
+};
 </script>
