fe8aded23d8f19b8a8e36f2f5444f7e2d90f2119
Gary Wu <s19003045@gmail.com>
2020-07-07 23:33:14 +0800

    關鍵字回應模組-前端-刪除模組(尚未完成，無法成功刪除)
    axios.delete 無法使用 req.body ，必須改用 query string 或 params 的方法

--- src/apis/keywordReply.js
+++ src/apis/keywordReply.js
@@ -3,11 +3,25 @@ import { apiHelper } from '../utils/helpers.js'
 export default {
   // 新增 module keyword
   createModuleKeyword({
-    params: botId,
+    params: {
+      botId: botId
+    },
     data: data
   }) {
     return apiHelper.post(`/bots/${botId}/module-keyword`, data)
   },
+  // 刪除 module keyword
+  deleteModuleKeyword({
+    params: {
+      botId: botId
+    },
+    data: data
+  }) {
+    console.log('data:', data)
+    //
+    return apiHelper.post(`/bots/${botId}/module-keyword`, data)
+  },
+
   // 取得關鍵字回應模組
   getKeywordReply(botId, ChatbotId) {
     return apiHelper.get(`/bots/${botId}/keyword-reply?ChatbotId=${ChatbotId}`, {
--- src/views/dashboard/components/components/EditorBotKeyword/ModuleList.vue
+++ src/views/dashboard/components/components/EditorBotKeyword/ModuleList.vue
@@ -10,11 +10,15 @@
         <tr v-for="moduleKeyword in moduleKeywords" v-bind:key="moduleKeyword.id">
           <td :data-module-keyword-uuid="moduleKeyword.uuid">
             <div class="mb-2">{{moduleKeyword.name === null ? "尚未命名模組" : moduleKeyword.name}}</div>
+            <button
+              class="btn btn-danger btn-sm"
+              @click="handleDeleteBtnClick(moduleKeyword.uuid)"
+            >刪除</button>
           </td>
         </tr>
       </tbody>
     </table>
-    <button class="btn btn-primary mb-3 ml-2 btn-sm" @click.stop.prevent="handleAddButtonClick">新增模組</button>
+    <button class="btn btn-primary mb-3 ml-2 btn-sm" @click.stop.prevent="handleAddBtnClick">新增模組</button>
   </div>
 </template>
 
@@ -35,7 +39,7 @@ export default {
   },
   created() {},
   methods: {
-    async handleAddButtonClick() {
+    async handleAddBtnClick() {
       //faked data
       const fakedDataCreateModule = {
         params: 1,
@@ -67,6 +71,48 @@ export default {
           title: "建立失敗，請稍後再試",
           text: ""
         });
+      }
+    },
+    async handleDeleteBtnClick(uuid) {
+      try {
+        console.log("uuid:", uuid);
+        //faked data
+        const fakedDataDeleteModule = {
+          params: {
+            botId: 1
+          },
+          data: {
+            ChatbotId: 1,
+            moduleKeyword: {
+              uuid: uuid
+            }
+          }
+        };
+
+        const { statusText, data } = await keywordReplyAPI.deleteModuleKeyword(
+          fakedDataDeleteModule
+        );
+
+        if (statusText === "OK") {
+          console.log("data:", data);
+
+          // 觸發父層事件 - $emit( '事件名稱' , 傳遞的資料 )
+          // this.$emit("after-create-module-keyword", data.data.moduleKeyword);
+
+          return Toast.fire({
+            icon: "success",
+            title: "成功刪除",
+            text: ""
+          });
+        } else {
+          return Toast.fire({
+            icon: "error",
+            title: "刪除失敗，請稍後再試",
+            text: ""
+          });
+        }
+      } catch (err) {
+        console.log(err);
       }
     }
   }
