960c23db7785f4530c797e07b55923d0f7150ca3
Gary Wu <s19003045@gmail.com>
2020-07-14 22:53:05 +0800

    分析模組-設定圖表之全域樣式

--- src/views/dashboard/components/AnalyticsTemplate.vue
+++ src/views/dashboard/components/AnalyticsTemplate.vue
@@ -25,6 +25,30 @@ import ModuleKeywordAnalysis from "./components/AnalyticsTemplate/ModuleKeywordA
 import ModulePostBackAnalysis from "./components/AnalyticsTemplate/ModulePostBackAnalysis.vue";
 import ReplyMessageAnalysis from "./components/AnalyticsTemplate/ReplyMessageAnalysis.vue";
 
+//chart default setting
+import Chart from "chart.js";
+Chart.defaults.global.defaultFontFamily = "'Noto Sans TC', 'Lato', sans-serif";
+Chart.defaults.global.defaultFontColor = "black";
+
+//setting for legend
+Chart.defaults.global.legend.labels.fontSize = 16;
+
+//setting for line
+//※Changing the global options only affects charts created after the change. Existing charts are not changed
+Chart.defaults.line.backgroundColor = "rgba(0, 0, 0, 0.1)";
+Chart.defaults.line.borderWidth = 5;
+Chart.defaults.line.fill = false;
+Chart.defaults.line.lineTension = 0;
+
+//setting for line option
+Chart.defaults.global.showLines = true;
+
+//setting for point in line
+Chart.defaults.global.elements.point.radius = 4;
+Chart.defaults.global.elements.point.hoverRadius = 8;
+Chart.defaults.global.elements.point.borderWidth = 2;
+Chart.defaults.global.elements.point.borderColor = "rgba(0, 0, 0, 0.3)";
+
 export default {
   name: "AnalyticsTemplate",
   components: {
