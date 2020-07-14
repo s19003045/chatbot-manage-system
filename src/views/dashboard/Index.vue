b148c0418db7e5801f7a17427e415518583d02c7
Gary Wu <s19003045@gmail.com>
2020-07-14 20:43:55 +0800

    分析模組-載入 google font (全域使用)

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -16,3 +16,15 @@ export default {
 };
 </script>
 
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@500&display=swap");
+
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap");
+
+* {
+  font-family: "Noto Sans TC", "Lato", sans-serif;
+}
+</style>
+
+
+
