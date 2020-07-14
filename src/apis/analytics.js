36f09e59854f3168bca20888ecf155c051db687a
Gary Wu <s19003045@gmail.com>
2020-07-14 14:14:24 +0800

    分析模組-建立路由

---
+++ src/apis/analytics.js
@@ -0,0 +1,48 @@
+import { apiHelper } from '../utils/helpers.js'
+
+export default {
+  //**********分析模組*********//
+  // 分析模組-取得關鍵字模組使用數據
+  getModuleKeywordAnalysis({
+    params: {
+      botId: botId
+    },
+    query: {
+      ChatbotId: ChatbotId
+    }
+  }) {
+    let queryString = `ChatbotId=${ChatbotId}`
+
+    return apiHelper.get(`/bots/${botId}/module-keyword-analysis?${queryString}`, {
+    })
+  },
+  // 分析模組-取得postback模組使用數據
+  getModulePostBackAnalysis({
+    params: {
+      botId: botId
+    },
+    query: {
+      ChatbotId: ChatbotId
+    }
+  }) {
+    let queryString = `ChatbotId=${ChatbotId}`
+
+    return apiHelper.get(`/bots/${botId}/module-postback-analysis?${queryString}`, {
+    })
+  },
+  // 分析模組-取得關鍵字模組使用數據
+  getReplyMessageAnalysis({
+    params: {
+      botId: botId
+    },
+    query: {
+      ChatbotId: ChatbotId
+    }
+  }) {
+    let queryString = `ChatbotId=${ChatbotId}`
+
+    return apiHelper.get(`/bots/${botId}/reply-message-analysis?${queryString}`, {
+    })
+  },
+
+}
