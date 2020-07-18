6426bf75e49747c82675b9a83523324a4dd7c108
Gary Wu <s19003045@gmail.com>
2020-07-19 00:02:51 +0800

    Postback 回應模組-建立 Text message component 之 text schema

--- src/views/dashboard/components/EditorBotPostBack/TextMessage.vue
+++ src/views/dashboard/components/EditorBotPostBack/TextMessage.vue
@@ -4,6 +4,96 @@
 
 <script>
 export default {
-  name: "TextMessage"
+  name: "TextMessage",
+  data() {
+    return {
+      textSchema: {
+        type: "text",
+        text: ""
+      },
+      quickReplySchema: {
+        items: [
+          {
+            type: "action", //required
+            imageUrl: "", //optional
+            action: {
+              type: "",
+              label: ""
+            }
+          }
+        ]
+      },
+      actionSchemaForQuick: {
+        postback: {
+          type: "postback",
+          label: "",
+          data: "subject=&data=",
+          text: ""
+        },
+        message: {
+          type: "message",
+          label: "",
+          text: ""
+        },
+        cameraRoll: {
+          type: "cameraRoll",
+          label: "Send photo"
+        },
+        location: {
+          type: "location",
+          label: "Location"
+        },
+        datetimepicker: {
+          type: "datetimepicker",
+          label: "Select date",
+          data: "storeId=12345",
+          mode: "datetime",
+          initial: "2017-12-25t00:00",
+          max: "2018-01-24t23:59",
+          min: "2017-12-25t00:00"
+        }
+      },
+      actionSchema: {
+        postback: {
+          type: "postback",
+          label: "",
+          data: "subject=&data=",
+          text: ""
+        },
+        message: {
+          type: "message",
+          label: "",
+          text: ""
+        },
+        uri: {
+          type: "uri",
+          label: "",
+          uri: "",
+          altUri: {
+            desktop: ""
+          }
+        },
+        cameraRoll: {
+          type: "cameraRoll",
+          label: "Send photo"
+        },
+        location: {
+          type: "location",
+          label: "Location"
+        },
+        datetimepicker: {
+          type: "datetimepicker",
+          label: "Select date",
+          data: "storeId=12345",
+          mode: "datetime",
+          initial: "2017-12-25t00:00",
+          max: "2018-01-24t23:59",
+          min: "2017-12-25t00:00"
+        }
+      },
+
+      context: []
+    };
+  },
 };
 </script>
