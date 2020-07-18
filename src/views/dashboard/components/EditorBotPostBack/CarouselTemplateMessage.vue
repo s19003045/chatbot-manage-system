0a8a1d6de2512285bdb1721f6c45f72e48f8db75
Gary Wu <s19003045@gmail.com>
2020-07-19 00:05:31 +0800

    Postback 回應模組- CarouselTemplateMessage  component-建立 Schema

--- src/views/dashboard/components/EditorBotPostBack/CarouselTemplateMessage.vue
+++ src/views/dashboard/components/EditorBotPostBack/CarouselTemplateMessage.vue
@@ -4,6 +4,30 @@
 
 <script>
 export default {
-  name: "CarouselTemplateMessage"
+  name: "CarouselTemplateMessage",
+  data() {
+    return {
+      schema: {
+        type: "template",
+        altText: "this is a carousel template",
+        template: {
+          type: "carousel",
+          actions: [],
+          columns: [
+            {
+              title: "",
+              text: "",
+              actions: [null, null]
+            },
+            {
+              title: "",
+              text: "",
+              actions: [null, null]
+            }
+          ]
+        }
+      }
+    };
+  }
 };
 </script>
