92e6f64e18aaf46221ba7117152cf033e8bd934e
Gary Wu <s19003045@gmail.com>
2020-07-19 23:59:21 +0800

    Postback 回應模組-建立 apiHelper- 新增 module postback

--- src/apis/postBackReply.js
+++ src/apis/postBackReply.js
@@ -1,3 +1,15 @@
+import { apiHelper } from '../utils/helpers.js'
+
+export default {
+  // 新增 postback module
+  createModulePostBack({
+    params: {
+      botId: botId
+    },
+    data: data
+  }) {
+    return apiHelper.post(`/bots/${botId}/module-postback`, data)
+  },
   // 取得回傳動作(postback)回應模組
   getPostBackReply({
     params: {
@@ -12,3 +24,5 @@
     return apiHelper.get(`/bots/${botId}/postback-reply?${queryString}`, {
     })
   },
+
+}
