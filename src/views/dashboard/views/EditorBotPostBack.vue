7fdc261675233a0251684f25bc41669ef9f637e0
Gary Wu <s19003045@gmail.com>
2020-07-20 00:16:58 +0800

    Postback 回應模組-建立測試用假資料

--- src/views/dashboard/views/EditorBotPostBack.vue
+++ src/views/dashboard/views/EditorBotPostBack.vue
@@ -105,8 +105,112 @@ export default {
   },
   data() {
     return {
-      modulePostBacks: [],
-      replyMessage: {},
+      //測試用假資料
+      modulePostBacks: [
+        {
+          ChatbotId: "1",
+          module: {
+            name: "點餐機-海陸大餐",
+            uuid: "75a0726b-41d1-41e3-ae6f-f5892a539516",
+            status: "in-use"
+          },
+          postBackEvents: [
+            {
+              name: "點餐機",
+              eventType: "postBack",
+              subject: "點餐機",
+              data: "海陸大餐"
+            }
+          ],
+          replyMessage: {
+            messageTemplate: {
+              type: "template",
+              altText: "this is a buttons template",
+              template: {
+                type: "buttons",
+                actions: [
+                  {
+                    type: "postback",
+                    label: "我要加價換套餐",
+                    displayText: "我要加價換套餐",
+                    data: "subject=海陸大餐&data=我要加價換套餐"
+                  },
+                  {
+                    type: "postback",
+                    label: "我不要加價換套餐",
+                    displayText: "我不要加價換套餐",
+                    data: "subject=海陸大餐&data=我不要加價換套餐"
+                  }
+                ],
+                title: "海陸大餐",
+                text: "是否要加價換套餐?"
+              }
+            },
+            type: "text",
+            name: "海陸大餐",
+            status: "in-use"
+          }
+        },
+        {
+          ChatbotId: "1",
+          module: {
+            name: "點餐機-素食大餐",
+            uuid: "ec36e1e9-fa4a-4017-b3c3-361ffa55e4e9",
+            status: "in-use"
+          },
+          PostBackEvent: {
+            name: "點餐機",
+            eventType: "postBack",
+            subject: "點餐機",
+            data: "素食大餐"
+          },
+          replyMessage: {
+            messageTemplate: {
+              type: "template",
+              altText: "this is a buttons template",
+              template: {
+                type: "buttons",
+                actions: [
+                  {
+                    type: "postback",
+                    label: "我要加價換套餐",
+                    displayText: "我要加價換套餐",
+                    data: "subject=素食大餐&data=我要加價換套餐"
+                  },
+                  {
+                    type: "postback",
+                    label: "我不要加價換套餐",
+                    displayText: "我不要加價換套餐",
+                    data: "subject=素食大餐&data=我不要加價換套餐"
+                  }
+                ],
+                title: "素食大餐",
+                text: "是否要加價換套餐?"
+              }
+            },
+            type: "text",
+            name: "素食大餐",
+            status: "in-use"
+          }
+        }
+      ],
+      //假資料
+      replyMessage: {
+        messageTemplate: [
+          {
+            type: "text",
+            text: "hello world"
+          },
+          {
+            type: "text",
+            text: "good job"
+          }
+        ],
+        type: "text",
+        name: "產前檢查",
+        uuid: "fab40e7a-76ba-4e04-9369-675b4327e875",
+        status: "in-use"
+      },
       postBackEvents: [],
       modulePostBack: {},
       moduleIndex: -1,
