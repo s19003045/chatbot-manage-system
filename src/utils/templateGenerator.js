5464ae6d4e2c7c49d102a153219f906a982ffb6c
Gary Wu <s19003045@gmail.com>
2020-07-20 18:24:14 +0800

    Postback 回應模組- 回應訊息編輯區-將各類訊息 Schema 模組化

---
+++ src/utils/templateGenerator.js
@@ -0,0 +1,29 @@
+'use strict'
+
+//載入 schema
+const textMessageSchema = require('./messageSchema/textMessageSchema.json')
+const confirmTemplateSchema = require('./messageSchema/confirmTemplateSchema.json')
+const buttonsTemplateSchema = require('./messageSchema/buttonsTemplateSchema.json')
+const carouselTemplateSchema = require('./messageSchema/carouselTemplateSchema.json')
+
+
+function templateGenerator(config) {
+  if (config.type === undefined) {
+    return new Error("config.type undefined !!")
+  }
+
+  switch (config.type) {
+    case 'text':
+      return textMessageSchema
+    case 'buttonsTemplate':
+      return buttonsTemplateSchema
+    case 'confirmTemplate':
+      return confirmTemplateSchema
+    case 'carouselTemplate':
+      return carouselTemplateSchema
+    default:
+      return new Error("We don't have such type!!")
+  }
+}
+
+module.exports = templateGenerator
