5c36c00e280ae7dd9f425ba3f4d11d091e22a1b3
Gary Wu <s19003045@gmail.com>
2020-07-27 15:30:31 +0800

    機器人腳本-刪除 replyModules 功能

--- src/apis/botScript.js
+++ src/apis/botScript.js
@@ -24,4 +24,18 @@ export default {
     return apiHelper.get(`/bots/${botId}/reply-module?${queryString}`, {
     })
   },
+  // 刪除 replyModule
+  deleteReplyModule({
+    params: {
+      botId: botId
+    },
+    query: {
+      ChatbotId: ChatbotId,
+      ReplyModuleUuid: ReplyModuleUuid
+    }
+  }) {
+    let queryString = `ChatbotId=${ChatbotId}&ReplyModuleUuid=${ReplyModuleUuid}`
+
+    return apiHelper.delete(`/bots/${botId}/reply-module?${queryString}`)
+  },
 }
