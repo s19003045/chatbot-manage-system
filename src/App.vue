22a1e9ca6e343471b8c8f0984d017fc278f77f2f
Gary Wu <s19003045@gmail.com>
2020-07-29 13:33:40 +0800

    Layout-建立 sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="manage-system">
     <Navbar />
+    <Sidebar />
     <main role="main" class="mt-5">
       <router-view />
     </main>
@@ -11,14 +12,23 @@
 // import "bootstrap";
 // import "bootswatch/dist/yeti/bootstrap.min.css";
 import Navbar from "./views/components/Navbar";
+import Sidebar from "./views/components/Sidebar";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    Sidebar,
   },
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@500&display=swap");
+
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap");
+
+* {
+  font-family: "Noto Sans TC", "Lato", sans-serif;
+}
 </style>
--- src/views/dashboard/components/core/Siebar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>Side bar</h1>
-</template>
-
-<script>
-export default {};
-</script>
