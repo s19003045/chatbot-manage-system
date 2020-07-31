32f6224667fde91bff5ce0e01f1f14d1c20a3ab8
Gary Wu <s19003045@gmail.com>
2020-07-31 22:22:29 +0800

    加入footer：版權聲明

---
+++ src/views/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container fluid class="bg-primary text-light py-2">
+    <b-row class="justify-content-center">
+      <b-col class="text-center">
+        <span text-muted>Copyright © 2020 MaraStar. All rights reserved</span>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -9,30 +9,12 @@
 // import { ToastLeave } from "../../utils/helpers";
 
 export default {
-  name: "App",
+  name: "Index",
   components: {},
-  data() {
-    return {
-      navTabSelect: "",
-    };
-  },
-  // beforeRouteUpdate(to, from, next) {
-  //   //跳出警示訊息，詢問是否確定要離開
-  //   ToastLeave.fire().then((result) => {
-  //     // 留在此頁面
-  //     if (!result.value) {
-  //       this.navTabSelect = from.name;
-  //       return;
-  //     }
-  //     //確定要離開，帶往下個頁面
-  //     if (result.value) {
-  //       this.navTabSelect = to.name;
-  //       next();
-  //     }
-  //   });
-  // },
 };
 </script>
+
+
 
 
 
