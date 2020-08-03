28d73ca160a4b3bb189b182f697fe330486f4036
Gary Wu <s19003045@gmail.com>
2020-08-04 00:14:05 +0800

    更換 bootstrap 樣式為 bootswatch-yeti

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootswatch/dist/yeti/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // fontawesome 
