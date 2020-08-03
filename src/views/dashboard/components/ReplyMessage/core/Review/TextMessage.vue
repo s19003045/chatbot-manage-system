8512ae29f7cf34e6ad20f6bea0eab5612149f906
Gary Wu <s19003045@gmail.com>
2020-08-03 20:35:21 +0800

    機器人腳本-預覽訊息-依訊息類別顯示-文字訊息-UI

--- src/views/dashboard/components/ReplyMessage/core/Review/TextMessage.vue
+++ src/views/dashboard/components/ReplyMessage/core/Review/TextMessage.vue
@@ -5,9 +5,9 @@
       <div class="avatar">
         <img src="https://picsum.photos/id/157/50/50" alt />
       </div>
-      <div class="message-template-item py-0 px-0 mr-auto mb-3 border-0 rounded bg-white centered">
+      <div class="message-template-item py-0 px-0 mr-auto mb-3 border-0 bg-white">
         <!-- 文字訊息 -->
-        <p class="py-2 px-3 my-0 rounded rounded-lg">{{replyMsgItem.text}}</p>
+        <p class="py-2 px-3 my-0">{{replyMsgItem.text}}</p>
       </div>
     </div>
   </div>
@@ -47,7 +47,7 @@ export default {
     border-radius: 15px;
 
     p {
-      border-radius: 5px;
+      border-radius: 15px;
       font-size: 0.9rem;
     }
   }
