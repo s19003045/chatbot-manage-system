9dab424944f1e2a4d2c8de87aaaf23d8f6d0421f
Gary Wu <s19003045@gmail.com>
2020-08-03 17:41:34 +0800

    機器人腳本-預覽訊息-依訊息類別顯示-文字訊息

---
+++ src/views/dashboard/components/ReplyMessage/core/Review/TextMessage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="row px-4 py-0">
+      <!-- avatar -->
+      <div class="avatar">
+        <img src="https://picsum.photos/id/157/50/50" alt />
+      </div>
+      <div class="message-template-item py-0 px-0 mr-auto mb-3 border-0 rounded bg-white centered">
+        <!-- 文字訊息 -->
+        <p class="py-2 px-3 my-0 rounded rounded-lg">{{replyMsgItem.text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    replyMsgItem: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.row {
+  background-color: #7ca2de;
+
+  .avatar {
+    height: 30px;
+    width: 30px;
+    vertical-align: middle;
+    margin: 0px 10px auto 0px;
+    border-radius: 50%;
+    img {
+      height: 30px;
+      width: 30px;
+      border-radius: 50%;
+    }
+  }
+  .message-template-item {
+    max-width: 80%;
+    border-radius: 15px;
+
+    p {
+      border-radius: 5px;
+      font-size: 0.9rem;
+    }
+  }
+}
+</style>
