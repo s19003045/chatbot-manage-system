888a2f784aa8856c7bccb9fceed52f6652256df0
Gary Wu <s19003045@gmail.com>
2020-08-03 17:05:05 +0800

    機器人腳本-預覽訊息-依訊息類別顯示

--- src/views/dashboard/components/ReplyMessage/core/Review.vue
+++ src/views/dashboard/components/ReplyMessage/core/Review.vue
@@ -5,47 +5,12 @@
     <b-modal id="reviewReplyMsg" title="預覽訊息" ok-only>
       <div class="container-fluid px-0 py-0 mx-0 chatting-wrapper custom-scrollbar-css">
         <!-- 多個 template 訊息顯示區 -->
-        <!-- 訊息1 -->
-        <div class="row px-4 pt-3 pb-2">
-          <div
-            class="message-template-item col-8 col-lg-8 py-0 px-0 mr-auto mb-3 border-0 rounded rounded-lg bg-light centered"
-          >
-            <div class="button-template-image rounded-top rounded-lg">
-              <!-- <img
-                      src="https://picsum.photos/id/1005/300/200"
-                      class="img-fluid mx-auto d-block rounded-top"
-                      alt="image display"
-              />-->
-            </div>
-
-            <h5 class="px-3 pt-3">服務選單-產前檢查</h5>
-            <hr class="my-0" />
-            <p class="template-btn-item text-primary text-center py-2 my-0">查詢健保給付項目</p>
-            <p class="template-btn-item text-primary text-center py-2 my-0">查詢自費項目</p>
-            <p class="template-btn-item text-primary text-center py-2 my-0">依週期數查詢產檢項目</p>
-          </div>
-        </div>
-        <!-- 訊息2 -->
-        <div class="row px-4 pt-3 pb-2">
-          <div
-            class="message-template-item col-8 col-lg-8 py-0 px-0 mr-auto mb-3 border-0 rounded rounded-lg bg-light centered"
-          >
-            <div class="button-template-image rounded-top rounded-lg">
-              <img
-                src="https://picsum.photos/id/106/300/200"
-                class="img-fluid mx-auto d-block rounded-top message-teplate-item-image"
-                alt="image display"
-              />
-            </div>
-
-            <h5 class="px-3 pt-3">服務選單-產前檢查</h5>
-            <hr class="my-0" />
-            <p class="template-btn-item text-primary text-center py-2 my-0">查詢健保給付項目</p>
-            <p class="template-btn-item text-primary text-center py-2 my-0">查詢自費項目</p>
-            <p class="template-btn-item text-primary text-center py-2 my-0">依週期數查詢產檢項目</p>
-          </div>
+        <!-- v-for -->
+        <div v-for="item in replyMessage" :key="item.id">
+          <ReviewSwitcher :reply-msg-item="item" />
         </div>
       </div>
+
       <!-- 使用者輸入框 -->
       <div class="row py-1 bg-light">
         <div class="col-12 boder border-primary d-flex justify-content-around align-items-center">
@@ -63,24 +28,25 @@
 </template>
 
 <script>
+import ReviewSwitcher from "./Review/ReviewSwitcher";
+
 export default {
   props: {
-    replyMessageItemReview: {
-      type: Object,
+    replyMessage: {
+      type: Array,
       default() {
-        return {};
+        return [];
       },
     },
+  },
+  components: {
+    ReviewSwitcher,
   },
 };
 </script>
 
 
 <style  lang="scss" scoped>
-.container-fluid .row {
-  background-color: #7ca2de;
-}
-
 .custom-scrollbar-css {
   scrollbar-width: thin;
   height: 300px;
@@ -103,36 +69,4 @@ export default {
     background-color: #8a8a8a;
   }
 }
-
-/* buttons template */
-.message-template-item {
-  border-radius: 20px !important;
-
-  .message-teplate-item-image {
-    border-top-left-radius: 20px !important;
-    border-top-right-radius: 20px !important;
-    background-size: cover;
-    background-position: center center;
-  }
-}
-// .message-teplate-item-image {
-//   border-top-left-radius: 20px !important;
-//   border-top-right-radius: 20px !important;
-// }
-
-/* template 中的 button */
-.template-btn-item:hover {
-  background-color: #ccc;
-
-  /* template 中的 button:last child */
-  &:last-child:hover {
-    border-bottom-left-radius: 20px;
-    border-bottom-right-radius: 20px;
-  }
-}
-// /* template 中的 button:last child */
-// .template-btn-item:last-child:hover {
-//   border-bottom-left-radius: 20px;
-//   border-bottom-right-radius: 20px;
-// }
 </style>
---
+++ src/views/dashboard/components/ReplyMessage/core/Review/ReviewSwitcher.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- text message -->
+    <div v-if="replyMsgItem.type === 'text'" class>
+      <TextMessage :reply-msg-item="replyMsgItem" />
+    </div>
+
+    <!-- button template message -->
+    <div v-if="replyMsgItem.type === 'template' && replyMsgItem.template.type === 'buttons'" class>
+      <ButtonsTemplate :reply-msg-item="replyMsgItem" />
+    </div>
+
+    <!-- carousel template message -->
+    <div v-if="replyMsgItem.type === 'template' && replyMsgItem.template.type === 'carousel'" class>
+      <CarouselTemplate :reply-msg-item="replyMsgItem" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TextMessage from "./TextMessage.vue";
+import ButtonsTemplate from "./ButtonsTemplate.vue";
+import CarouselTemplate from "./CarouselTemplate.vue";
+
+export default {
+  props: {
+    replyMsgItem: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  components: {
+    TextMessage,
+    ButtonsTemplate,
+    CarouselTemplate,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
