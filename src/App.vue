8d2dc13ecc144b1c0f44f5f8b580d97a3e1ca2f5
Gary Wu <s19003045@gmail.com>
2020-08-04 11:26:02 +0800

    機器人腳本-模組列表-調整全域 btn 樣式

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@500&display=swap");
 
 @import url("https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap");
@@ -74,5 +74,13 @@ export default {
 
 main {
   min-height: 100%;
+}
+
+button.btn {
+  border-radius: 3px;
+}
+
+button.rounded {
+  border-radius: 5px;
 }
 </style>
