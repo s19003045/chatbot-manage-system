60b4d09450ab1809501660c1ee06386a9f5c1308
Gary Wu <s19003045@gmail.com>
2020-08-05 11:11:21 +0800

    機器人腳本- 關鍵字編輯區-删除關鍵字

--- src/views/dashboard/components/EditorBotScript/KeywordEditor.vue
+++ src/views/dashboard/components/EditorBotScript/KeywordEditor.vue
@@ -2,52 +2,33 @@
   <div>
     <!-- 關鍵字設定 -->
     <div class="mb-5 py-2 px-2 border border-secondary rounded shadow-lg">
-      <h5
-        class="mb-4 py-2 px-3 bg-secondary text-dark border border-secondary rounded"
-      >
-        關鍵字編輯
-      </h5>
+      <h5 class="mb-4 py-2 px-3 bg-secondary text-dark border border-secondary rounded">關鍵字編輯</h5>
       <!-- 關鍵字編輯區，點擊模組後才可以編輯 -->
-      <div v-if="moduleClick.status" class="">
+      <div v-if="moduleClick.status" class>
         <div v-if="keywords" class>
           <div v-for="(keyword, index) in keywords" :key="index" class="mb-3">
             <div class="row mb-2">
               <!-- 是否使用此關鍵字 -->
               <div class="input-group col-8 col-sm-8">
-                <label for="keyword-status" class="input-group-text"
-                  >是否啟用</label
-                >
-                <select
-                  name=""
-                  id="keyword-status"
-                  class="custom-select"
-                  v-model="keyword.status"
-                >
-                  <option value="edited" :selected="keyword.status === 'edited'"
-                    >edited</option
-                  >
-                  <option value="in-use" :selected="keyword.status === 'in-use'"
-                    >in-use</option
-                  >
+                <label for="keyword-status" class="input-group-text">是否啟用</label>
+                <select name id="keyword-status" class="custom-select" v-model="keyword.status">
+                  <option value="edited" :selected="keyword.status === 'edited'">edited</option>
+                  <option value="in-use" :selected="keyword.status === 'in-use'">in-use</option>
                 </select>
               </div>
               <!-- 刪除按鍵 -->
               <div class="col-4 col-sm-4 ml-auto d-flex justify-content-end">
                 <button
                   class="btn btn-outline-danger"
-                  @click.prevent.stop="handleDeleteKeywordBtnClick(keyword.id)"
-                >
-                  刪除
-                </button>
+                  @click.prevent.stop="handleDeleteKeywordBtnClick(index)"
+                >刪除</button>
               </div>
             </div>
 
             <!-- 顯示關鍵字名稱 -->
             <div class="input-group mb-2">
               <div class="input-group-prepend">
-                <label for="keywordName" class="input-group-text"
-                  >關鍵字名稱：</label
-                >
+                <label for="keywordName" class="input-group-text">關鍵字名稱：</label>
               </div>
               <input
                 type="text"
@@ -61,18 +42,13 @@
         </div>
       </div>
       <div v-if="moduleClick.status" class="mb-5 mt-4">
-        <button
-          class="btn btn-primary mb-2 mr-3"
-          @click="handleAddKeywordBtnClick"
-        >
-          新增關鍵字
-        </button>
+        <button class="btn btn-primary mb-2 mr-3" @click="handleAddKeywordBtnClick">新增關鍵字</button>
         <!-- 顯示關鍵字數 -->
-        <span class="text-muted"
-          >關鍵字數(啟用/全部) ：{{ activatedKeywordCount }}/{{
-            keywords.length
-          }}</span
-        >
+        <span class="text-muted">
+          關鍵字數(啟用/全部) ：{{ activatedKeywordCount }}/{{
+          keywords.length
+          }}
+        </span>
       </div>
     </div>
   </div>
@@ -165,11 +141,52 @@ export default {
       }
     },
     //點擊刪除關鍵字
-    handleDeleteKeywordBtnClick(keywordId) {
-      console.log(keywordId);
-      // 檢查請求需要的資料
-      // 發送請求
-      // 判斷 response
+    async handleDeleteKeywordBtnClick(index) {
+      try {
+        this.isProcessing = true;
+        // 檢查請求需要的資料
+        const apiData = {
+          params: {
+            botId: this.$store.state.chatbot.botId,
+          },
+          query: {
+            ChatbotId: 1,
+            keywordUuid: this.keywords[index].uuid,
+          },
+          data: {},
+        };
+
+        // 發送請求
+        const { statusText, data } = await keywordAPI.deleteKeyword(apiData);
+
+        if (statusText === "OK" && data.status === "success") {
+          this.isProcessing = false;
+          //刪除該筆資料
+          this.keywords.splice(index, 1);
+
+          return Toast.fire({
+            icon: "success",
+            title: "存取成功",
+            text: "",
+          });
+        } else {
+          this.isProcessing = false;
+          return Toast.fire({
+            icon: "error",
+            title: "存取失敗，請稍後再試",
+            text: "",
+          });
+        }
+
+        // 判斷 response
+      } catch (err) {
+        this.isProcessing = false;
+        return Toast.fire({
+          icon: "error",
+          title: "存取失敗，請稍後再試",
+          text: `${err.message}`,
+        });
+      }
     },
   },
 };
